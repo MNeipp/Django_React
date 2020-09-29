479382d990b14e45df70b5eef12b7755d780af22
MNeipp <ReamusLQ@gmail.com>
2020-09-28 18:58:36 -0600

    initial commit, django_react

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import List from './List'
+
+function App(){
+    return(
+        <List/>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'))
---
+++ users/api.py
@@ -0,0 +1,10 @@
+from .models import User
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permissions = [
+        permissions.AllowAny
+    ]
+    serializer_class = UserSerializer
