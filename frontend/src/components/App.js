3b2737640f7d54f52efdee89ae52848f3a35c9e0
MNeipp <ReamusLQ@gmail.com>
2020-09-29 19:03:33 -0600

    integrated self API

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,77 @@
-import React from 'react'
+  
+import React, { useEffect, useState, useMemo } from 'react';
 import ReactDOM from 'react-dom'
-import List from './List'
+import { Container } from 'reactstrap';
+import TableContainer from './TableContainer';
+import axios from 'axios'
 
-function App(){
-    return(
-        <List/>
+
+const App = () => {
+    const [data, setData] = useState(null);
+    // useEffect(() => {
+    //     axios.get('/api/users')
+    //     .then(response => setData(response.data));
+    //   }, []);
+    useEffect(() => {
+      const doFetch = async () => {
+        const response = await fetch("/api/users")
+        // const response = await fetch("https://randomuser.me/api/?results=100")
+        const body = await response.json()
+        setData(body);
+      }
+      doFetch()
+    }, [])
+    
+
+//   const columns = useMemo(
+//     () => [
+//       {
+//         Header: 'First Name',
+//         accessor: 'first_name',
+//       },
+//       {
+//         Header: 'Last Name',
+//         accessor: 'last_name',
+//       },
+//       {
+//         Header: 'Email',
+//         accessor: 'email',
+//       },
+    
+//     ],
+//     [] );
+    const columns = useMemo(
+        () => [
+        {
+            Header: "Title",
+            accessor: "name.title",
+        },
+        {
+            Header: "First Name",
+            accessor: "name.first",
+        },
+        {
+            Header: "Last Name",
+            accessor: "name.last",
+        },
+        {
+            Header: "Email",
+            accessor: "email",
+        },
+        {
+            Header: "City",
+            accessor: "location.city",
+        },
+        ],
+        []
     )
-}
+
+    return (
+        <Container style={{ marginTop: 100}}>
+            <h1>This will render!</h1>
+            {data  && <TableContainer columns={columns} users={data} />}
+        </Container>
+    );
+};
 
 ReactDOM.render(<App/>, document.getElementById('app'))
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
 from rest_framework import routers
 from .api import UserViewSet
+from . import views
+from django.urls import path
 
 router = routers.DefaultRouter()
 router.register('api/users', UserViewSet, 'users')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', views.index)
+]
+
+urlpatterns += router.urls
